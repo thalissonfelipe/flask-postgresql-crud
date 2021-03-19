31d801b183e468d3306a823bdee8accd68ae0dd9
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-18 22:18:01 -0300

    feat: add get users route

--- backend/blueprints/users.py
+++ backend/blueprints/users.py
@@ -1,12 +1,25 @@
 from models.models import User
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 
 users_blueprint = Blueprint('users_blueprint', __name__)
 
 
-@users_blueprint.route('/', methods=['GET'])
+@users_blueprint.route('/users', methods=['GET'])
 def index():
-    users = User.query.all()
+    if request.method == 'GET':
+        limit = request.args.get('limit', None)
+        order_by = request.args.get('order_by', None)
+        filter_obj = {}
 
-    return jsonify([user.serialize() for user in users])
+        for key in request.args:
+            if key not in ['limit', 'order_by']:
+                filter_obj[key] = request.args[key]
+
+        users = User.query \
+                    .filter_by(**filter_obj) \
+                    .order_by(order_by) \
+                    .limit(limit) \
+                    .all()
+
+        return jsonify([user.serialize() for user in users])
