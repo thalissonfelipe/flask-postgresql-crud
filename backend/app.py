a715bcccd05a91971da744e7d47160690bec6f3c
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-18 23:35:46 -0300

    feat: add error handlers

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask
 from database.db import db
 from dotenv import load_dotenv
+from flask import Flask, Response
 from blueprints.users import users_blueprint
 
 load_dotenv()  # load env files
@@ -13,6 +13,21 @@ def create_app():
     db.init_app(app)
 
     app.register_blueprint(users_blueprint)
+
+    @app.errorhandler(404)
+    def route_not_found(e):
+        response = Response(response='Route not found.', status=404)
+        return response
+
+    @app.errorhandler(405)
+    def method_not_allowed(e):
+        response = Response(response='Method not allowed.', status=405)
+        return response
+
+    @app.errorhandler(Exception)
+    def internal_error(e):
+        response = Response(response='Internal Error.', status=500)
+        return response
 
     return app
 
