3c82984ce0a95a31e2f3202cb8bb6430e374878f
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-18 21:21:19 -0300

    feat: add users and addresses relationship

--- backend/models/models.py
+++ backend/models/models.py
@@ -4,23 +4,71 @@ from database.db import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    age = db.Column(db.Integer())
-    address = db.Column(db.String())
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    address = db.relationship('Address', uselist=False, backref='users')
 
-    def __init__(self, name, age, address):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.address = address
 
     def __repr__(self):
-        return '<User %r>' % self.name
+        return "User(name='{}', age={}, address={})".format(
+            self.name,
+            self.age,
+            self.address
+        )
 
     def serialize(self):
         return {
             'id': self.id,
             'name': self.name,
             'age': self.age,
-            'address': self.address
+            'address': {
+                'street': self.address.street,
+                'number': self.address.number,
+                'city': self.address.city,
+                'state': self.address.state
+            }
+        }
+
+
+class Address(db.Model):
+    __tablename__ = 'addresses'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    street = db.Column(db.String, nullable=False)
+    number = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    state = db.Column(db.String, nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        unique=True,
+        nullable=False
+    )
+
+    def __init__(self, street, number, city, state, user_id):
+        self.street = street
+        self.number = number
+        self.city = city
+        self.state = state
+        self.user_id = user_id
+
+    def __repr__(self):
+        return "Address(street='{}', number={}, city='{}', state='{}')".format(
+            self.street,
+            self.number,
+            self.city,
+            self.state
+        )
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'street': self.street,
+            'number': self.number,
+            'city': self.city,
+            'state': self.state
         }
