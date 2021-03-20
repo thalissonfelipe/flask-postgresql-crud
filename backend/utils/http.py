55626cdbd1edb868a613a606ee079214f15df1c1
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-03-19 23:46:40 -0300

    docs: add type hints and doc strings

--- backend/utils/http.py
+++ backend/utils/http.py
@@ -1,28 +1,63 @@
+from database.db import db
+from typing import List, Union
 from models.models import User
 from flask import Response, jsonify
+from werkzeug.wrappers import Response as ResponseType
 
 
-def ok(resource):
+def ok(resource: Union[db.Model, List[db.Model]]) -> ResponseType:
+    """
+    Helper function that returns an http status code 200 and the
+    serialized model.
+
+    @param resource: Model or a list of models to be serialized
+    """
+
     if isinstance(resource, User):
         return jsonify(resource.serialize())
     return jsonify([res.serialize() for res in resource])
 
 
-def no_content():
+def no_content() -> ResponseType:
+    """
+    Helper function that returns an http status code 204.
+    """
+
     return Response(status=204)
 
 
-def created(model):
+def created(model: db.Model) -> ResponseType:
+    """
+    Helper function that returns an http status code 201 and the
+    serialized model.
+
+    @param model: Model to be serialized
+    """
+
     return jsonify(model.serialize()), 201
 
 
-def not_found(resource):
+def not_found(resource: str) -> ResponseType:
+    """
+    Helper function that returns an http status code 404.
+
+    @param resource: Resource name that was not found.
+    """
+
     return Response(response=f'{resource} not found.'.capitalize(), status=404)
 
 
-def not_allowed():
+def not_allowed() -> ResponseType:
+    """
+    Helper function that returns an http status code 405.
+    """
+
     return Response(response='Method not allowed.', status=405)
 
 
-def internal_error():
+def internal_error() -> ResponseType:
+    """
+    Helper function that returns an http status code 500.
+    """
+
     return Response(response='Internal Error.', status=500)
